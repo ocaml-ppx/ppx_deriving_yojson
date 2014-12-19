039c3c34baff8d604c203b84ab96b8ac7d14b59d
Zoggy <zoggy@bat8.org>
2014-12-19 16:13:23 +0100

    fix access to field

--- src/ppx_deriving_yojson.ml
+++ src/ppx_deriving_yojson.ml
@@ -256,7 +256,7 @@ let ser_str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
             in
             let flid = lid (Printf.sprintf "%s.f" mod_name) in
             let set_field = Exp.setfield (Exp.ident flid) flid [%expr Obj.magic f] in
-            let field = Exp.ident (lid (Printf.sprintf "%s.f.%s.f" mod_name mod_name)) in
+            let field = Exp.field (Exp.ident flid) (flid) in
             let extend = Str.value Nonrecursive
               [Vb.mk (pvar extend_name) [%expr fun fn ->
                    let f (*: [%t ty]*) = fn [%e field] in
@@ -439,7 +439,7 @@ let desu_str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
             in
             let flid = lid (Printf.sprintf "%s.f" mod_name) in
             let set_field = Exp.setfield (Exp.ident flid) flid [%expr Obj.magic f] in
-            let field = Exp.ident (lid (Printf.sprintf "%s.f.%s.f" mod_name mod_name)) in
+            let field = Exp.field (Exp.ident flid) flid in
             let extend = Str.value Nonrecursive
                 [Vb.mk (pvar extend_name) [%expr fun fn ->
                     let f (*: [%t ty]*) = fn [%e field] in
