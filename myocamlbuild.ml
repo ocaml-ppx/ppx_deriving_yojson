f6dcebf5668c0ae4c393a60b1ef3ae175862e095
whitequark <whitequark@whitequark.org>
2015-11-09 13:39:37 +0300

    Fix tests.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,14 +3,19 @@ open Ocamlbuild_plugin
 let () = dispatch (
   function
   | After_rules ->
-    let std_deriver deriver =
+    let ppx_deriving_component deriver =
       (Findlib.query "ppx_deriving").Findlib.location ^ "/" ^ deriver
     in
     flag ["ocaml"; "compile"; "use_yojson"] &
       S[A"-ppx"; A"ocamlfind ppx_import/ppx_import";
         A"-ppx"; A("ocamlfind ppx_deriving/ppx_deriving "^
                    "src/ppx_deriving_yojson.cma "^
-                   (std_deriver "ppx_deriving_show.cma"))];
+                   (ppx_deriving_component "ppx_deriving_show.cma"));
+        A"-I"; A(ppx_deriving_component "")];
+    flag ["ocaml"; "link"; "use_yojson"; "byte"] &
+      A(ppx_deriving_component "ppx_deriving_runtime.cma");
+    flag ["ocaml"; "link"; "use_yojson"; "native"] &
+      A(ppx_deriving_component "ppx_deriving_runtime.cmxa");
     ocaml_lib ~dir:"src" "src/ppx_deriving_yojson_runtime";
 
   | _ -> ())
