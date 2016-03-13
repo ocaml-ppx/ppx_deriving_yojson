e3807ec7c50501035468e1f53b6fb83e147ae6ae
Rudi Grinberg <rudi.grinberg@gmail.com>
2016-03-13 19:23:19 -0400

    Switch to using result
    Switch from polymorphic variant version of result to the standard one.

--- src/ppx_deriving_yojson_runtime.ml
+++ src/ppx_deriving_yojson_runtime.ml
@@ -1,19 +1,24 @@
+include Ppx_deriving_runtime
+
 let (>>=) x f =
-  match x with `Ok x -> f x | (`Error _) as x -> x
+  match x with Result.Ok x -> f x | (Result.Error _) as x -> x
 
 let (>|=) x f =
-  x >>= fun x -> `Ok (f x)
+  x >>= fun x -> Result.Ok (f x)
 
 let rec map_bind f acc xs =
   match xs with
   | x :: xs -> f x >>= fun x -> map_bind f (x :: acc) xs
-  | [] -> `Ok (List.rev acc)
+  | [] -> Result.Ok (List.rev acc)
 
+type 'a error_or = ('a, string) Result.result
 
-module List = List
-module String = String
-module Bytes = Bytes
-module Int32 = Int32
-module Int64 = Int64
-module Nativeint = Nativeint
-module Array = Array
+let show_error_or f = function
+  | Result.Error s -> "Error: " ^ s
+  | Result.Ok x -> f x
+
+let pp_error_or pp_x fmt = function
+  | Result.Ok x -> pp_x fmt x
+  | Result.Error e ->
+    Format.pp_print_string fmt "Error ";
+    Format.pp_print_string fmt e
