8ca89043b0befdd32014c362981d9d3e26bd20fe
Rudi Grinberg <rudi.grinberg@gmail.com>
2016-03-30 21:28:40 -0400

    Remove hand written show for error_or
    Not needed and buggy

--- src/ppx_deriving_yojson_runtime.ml
+++ src/ppx_deriving_yojson_runtime.ml
@@ -12,13 +12,3 @@ let rec map_bind f acc xs =
   | [] -> Result.Ok (List.rev acc)
 
 type 'a error_or = ('a, string) Result.result
-
-let show_error_or f = function
-  | Result.Error s -> "Error: " ^ s
-  | Result.Ok x -> f x
-
-let pp_error_or pp_x fmt = function
-  | Result.Ok x -> pp_x fmt x
-  | Result.Error e ->
-    Format.pp_print_string fmt "Error ";
-    Format.pp_print_string fmt e
