b2500fd5d4f3b8bb0b925158d49587ff69269868
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-03-12 12:18:16 -0700

    Drop the `result` compatibility package dependency

--- src/ppx_deriving_yojson_runtime.mli
+++ src/ppx_deriving_yojson_runtime.mli
@@ -1,4 +1,4 @@
-type 'a error_or = ('a, string) Result.result
+type 'a error_or = ('a, string) result
 
 val ( >>= ) : 'a error_or -> ('a -> 'b error_or) -> 'b error_or
 val ( >|= ) : 'a error_or -> ('a -> 'b) -> 'b error_or
@@ -17,8 +17,3 @@ module Int32 : (module type of Int32)
 module Int64 : (module type of Int64)
 module Nativeint : (module type of Nativeint)
 module Array : (module type of Array)
-module Result : sig
-  type ('a, 'b) result = ('a, 'b) Result.result =
-    | Ok of 'a
-    | Error of 'b
-end
